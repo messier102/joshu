d9c6370514f4d748ab24bb04ed7b6a5d9e3bef06
messier102 <messier102@fastmail.com>
2021-04-15 22:32:13 +0700

    Implement basic quoted arguments

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -8,9 +8,21 @@ export class Command {
     ) {}
 
     static from_raw_message(message: Discord.Message, prefix: string): Command {
-        const args = message.content.slice(prefix.length).split(" ");
+        const message_stripped = message.content.slice(prefix.length);
+        const args = parse_arguments(message_stripped);
         const [command_name, ...command_args] = args;
 
         return new Command(command_name, command_args, message);
     }
+}
+
+function parse_arguments(message: string): string[] {
+    const contiguous_or_quoted = /([^\s"]+)|"([^"]*)"|'([^']*)'/gi;
+
+    // extract the arguments from capture groups
+    const args = [...message.matchAll(contiguous_or_quoted)].map(
+        (match) => match[1] ?? match[2] ?? match[3]
+    );
+
+    return args;
 }
