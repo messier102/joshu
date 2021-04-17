f43305fca641f9080391881c8c7d16bef9704b7d
messier102 <messier102@fastmail.com>
2021-04-17 17:47:59 +0700

    Replace live link with archive version

--- src/commands/type_guards/snowflake.ts
+++ src/commands/type_guards/snowflake.ts
@@ -5,7 +5,7 @@ export class SnowflakeArgument implements ArgumentTypeGuard {
 
     is_valid_argument(arg: string): boolean {
         // discord snowflakes are at least 17 digits long
-        // see https://old.reddit.com/r/discordapp/comments/a5wtl4/lowest_id_on_discord/ebq39g4/
+        // see https://archive.md/nvaQA
         const snowflake_regex = /^\d{17,}$/;
 
         return snowflake_regex.test(arg);
