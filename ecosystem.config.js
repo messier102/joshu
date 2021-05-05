9ffc7372e246baf77b50486b5fe08e40e904b642
messier102 <messier102@fastmail.com>
2021-05-05 14:57:39 +0700

    Add deployment pipeline

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [
+        {
+            name: "Joshu",
+            script: "build/src/index.js",
+        },
+    ],
+
+    deploy: {
+        production: {
+            user: process.env.PROD_USER,
+            host: process.env.PROD_HOST,
+            key: "./production.key",
+            ref: "origin/master",
+            repo: "git@github.com:messier102/joshu.git",
+            path: "/home/ubuntu/joshu",
+            "pre-deploy": "node -v && npm -v",
+            "post-deploy":
+                "mkdir -p ./data && \
+                cp /home/ubuntu/config.ts ./data/config.ts && \
+                npm run build-production && \
+                pm2 reload ecosystem.config.js --env production",
+        },
+    },
+};
