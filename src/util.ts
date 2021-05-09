f4c7481ba93a13d486eb048353cb3357ac8710e3
messier102 <messier102@fastmail.com>
2021-05-09 14:13:17 +0700

    Document `range`

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,14 @@
+/**
+ * Creates an iterable range of numeric values from `start` (inclusive)
+ * to `end` (exclusive). Useful for defining for-loops.
+ *
+ * @param start The starting of the range (inclusive)
+ * @param end The ending value of the range (exclusive)
+ * @param step The distance between two consequtive values (default 1).
+ *
+ * @example
+ * for (const i of range(0, 5)) { ... }
+ */
 export function* range(
     start: number,
     end: number,
