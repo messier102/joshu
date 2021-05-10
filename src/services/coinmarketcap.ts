5459339b2aef8b5955a4a2f560f85f1f40d44811
messier102 <messier102@fastmail.com>
2021-05-10 13:00:17 +0700

    Replace node-fetch with axios for cleaner API

--- src/services/coinmarketcap.ts
+++ src/services/coinmarketcap.ts
@@ -1,6 +1,6 @@
-import fetch from "node-fetch";
 import { Err, Ok, Result } from "ts-results";
 import config from "../../data/config";
+import axios from "axios";
 
 type Conversions = {
     [key: string]: {
@@ -28,27 +28,36 @@ type CoinMarketCapResponse = {
     };
 };
 
+const COINMARKETCAP_API = axios.create({
+    baseURL: `https://${config.cmc_domain}/`,
+    headers: {
+        "X-CMC_PRO_API_KEY": config.cmc_api_key,
+    },
+
+    // API always responds with a JSON status object regardless of the HTTP code
+    validateStatus: () => true,
+});
+
 export async function convert_currency(
     base_currency: string,
     target_currency: string,
     amount: number
 ): Promise<Result<Conversions, string>> {
-    const api_endpoint =
-        `https://${config.cmc_domain}/v1/tools/price-conversion?` +
-        `symbol=${base_currency}&` +
-        `convert=${target_currency}&` +
-        `amount=${amount}`;
+    const response = await COINMARKETCAP_API.get<CoinMarketCapResponse>(
+        "/v1/tools/price-conversion",
+        {
+            params: {
+                symbol: base_currency,
+                convert: target_currency,
+                amount,
+            },
+        }
+    );
+    const json = response.data;
 
-    const response = await fetch(api_endpoint, {
-        headers: {
-            "X-CMC_PRO_API_KEY": config.cmc_api_key,
-        },
-    });
-    const body: CoinMarketCapResponse = await response.json();
-
-    if (response.ok && body.data) {
-        return Ok(body.data.quote);
+    if (json.data) {
+        return Ok(json.data.quote);
     } else {
-        return Err(body.status.error_message);
+        return Err(json.status.error_message);
     }
 }
