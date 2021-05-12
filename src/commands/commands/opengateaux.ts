880e245939aaf1ab8bfc331b6163b3aab93aa986
messier102 <messier102@fastmail.com>
2021-05-12 12:59:37 +0700

    Convert `opengateaux` from `snoowrap` to `snoots`

--- src/commands/commands/opengateaux.ts
+++ src/commands/commands/opengateaux.ts
@@ -2,10 +2,20 @@ import { CommandRequest } from "../request";
 import { CommandParameter, Command } from "../command";
 import { Permissions } from "discord.js";
 import StringConverter from "../type_converters/StringConverter";
-import snoowrap from "snoowrap";
 import config from "../../../data/config";
+import RedditClient from "snoots";
 
-const reddit = new snoowrap(config.reddit);
+const reddit = new RedditClient({
+    userAgent: config.reddit.userAgent,
+    creds: {
+        clientId: config.reddit.clientId,
+        clientSecret: config.reddit.clientSecret,
+    },
+    auth: {
+        username: config.reddit.username,
+        password: config.reddit.password,
+    },
+});
 
 export default <Command>{
     parameters: [new CommandParameter("post title", StringConverter)],
@@ -13,34 +23,35 @@ export default <Command>{
 
     accept_remainder_arg: true,
 
-    execute({ source }: CommandRequest, post_title: string): void {
-        source.guild?.systemChannel
-            ?.createInvite({
-                unique: true,
-            })
-            .then((invite) => {
-                if (!(invite && invite.url)) {
-                    console.log("unable to create invite");
-                    throw new Error("unable to create invite");
-                }
+    async execute(
+        { source }: CommandRequest,
+        post_title: string
+    ): Promise<void> {
+        const invite = await source.guild?.systemChannel?.createInvite({
+            unique: true,
+        });
 
-                return reddit
-                    .submitLink({
-                        subredditName: config.subreddit,
-                        title: post_title,
-                        url: invite.url,
-                    })
-                    .then((post) => post.fetch())
-                    .then((post) => post.unmarkNsfw())
-                    .then((post) =>
-                        source.channel.send(
-                            `Opened the gates: https://www.reddit.com${post.permalink}`
-                        )
-                    )
-                    .catch((reason) => {
-                        source.reply(`Reddit error: \`${reason.toString()}\``);
-                        console.log(reason);
-                    });
-            });
+        if (!invite) {
+            console.log("unable to create invite");
+            throw new Error("unable to create invite");
+        }
+
+        try {
+            const post_id = await reddit.subreddits.postLink(
+                config.subreddit,
+                post_title,
+                invite.url
+            );
+
+            const post = await reddit.posts.fetch(post_id);
+            await post.unmarkNsfw();
+
+            source.channel.send(
+                `Opened the gates: https://www.reddit.com${post.permalink}`
+            );
+        } catch (reason) {
+            source.reply(`Reddit error: \`${reason.toString()}\``);
+            console.log(reason);
+        }
     },
 };
