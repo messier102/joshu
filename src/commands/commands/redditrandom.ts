dd3d6a1970f71300ab69b3adea12ca0088e4ebe0
messier102 <messier102@fastmail.com>
2021-05-12 13:05:14 +0700

    Convert `redditrandom` from `snoowrap` to `snoots`

--- src/commands/commands/redditrandom.ts
+++ src/commands/commands/redditrandom.ts
@@ -1,23 +1,39 @@
 import { CommandRequest } from "../request";
 import { CommandParameter, Command } from "../command";
-import snoowrap from "snoowrap";
-
+import RedditClient from "snoots";
 import config from "../../../data/config";
 import StringConverter from "../type_converters/StringConverter";
 
-const reddit = new snoowrap(config.reddit);
+const reddit = new RedditClient({
+    userAgent: config.reddit.userAgent,
+    creds: {
+        clientId: config.reddit.clientId,
+        clientSecret: config.reddit.clientSecret,
+    },
+    auth: {
+        username: config.reddit.username,
+        password: config.reddit.password,
+    },
+});
 
 export default <Command>{
     parameters: [new CommandParameter("subreddit", StringConverter)],
     permissions: [],
 
-    execute({ source }: CommandRequest, subreddit: string): void {
-        reddit
-            .getRandomSubmission(subreddit)
-            .then((post) =>
-                source.channel.send(
-                    `Random reddit post from \`r/${subreddit}\`:\nhttps://www.reddit.com${post.permalink}`
-                )
+    async execute(
+        { source }: CommandRequest,
+        subreddit: string
+    ): Promise<void> {
+        try {
+            const random_post = await reddit.subreddits.getRandomPost(
+                subreddit
             );
+
+            source.channel.send(
+                `Random reddit post from \`r/${subreddit}\`:\nhttps://www.reddit.com${random_post.permalink}`
+            );
+        } catch (e) {
+            source.reply("sorry, couldn't fetch that subreddit.");
+        }
     },
 };
