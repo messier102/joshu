bbc135a10935c89f693c580a98035525babbe5b0
messier102 <messier102@fastmail.com>
2021-05-14 12:28:53 +0700

    Implement deleting old invites on opengateaux

--- src/commands/commands/opengateaux.ts
+++ src/commands/commands/opengateaux.ts
@@ -7,7 +7,10 @@ import { reddit } from "../../services/reddit";
 
 export default <Command>{
     parameters: [new CommandParameter("post title", StringConverter)],
-    permissions: [Permissions.FLAGS.CREATE_INSTANT_INVITE],
+    permissions: [
+        Permissions.FLAGS.CREATE_INSTANT_INVITE,
+        Permissions.FLAGS.MANAGE_GUILD,
+    ],
 
     accept_remainder_arg: true,
 
@@ -15,11 +18,23 @@ export default <Command>{
         { source }: CommandRequest,
         post_title: string
     ): Promise<void> {
-        const invite = await source.guild?.systemChannel?.createInvite({
+        if (!source.guild) {
+            source.reply("sorry, this can only be done in a server.");
+            return;
+        }
+
+        const old_invites = await source.guild.fetchInvites();
+        for (const [_, old_invite] of old_invites) {
+            if (old_invite.inviter === source.client.user) {
+                old_invite.delete();
+            }
+        }
+
+        const new_invite = await source.guild.systemChannel?.createInvite({
             unique: true,
         });
 
-        if (!invite) {
+        if (!new_invite) {
             console.log("unable to create invite");
             throw new Error("unable to create invite");
         }
@@ -29,7 +44,7 @@ export default <Command>{
             const old_posts = bot_user.getPosts();
 
             for await (const old_post of old_posts) {
-                // A workaround for a bug in snoots which causes `getPosts()` to
+                // A workaround for a bug in snoots dev.12 which causes `getPosts()` to
                 // yield `undefined` if the user has no submissions.
                 if (!old_post) {
                     continue;
@@ -41,7 +56,7 @@ export default <Command>{
             const new_post_id = await reddit.subreddits.postLink(
                 config.reddit.opengateaux_subreddit,
                 post_title,
-                invite.url
+                new_invite.url
             );
 
             const new_post = await reddit.posts.fetch(new_post_id);
