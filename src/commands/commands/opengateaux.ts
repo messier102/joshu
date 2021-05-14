f2d88e5ebfca015c0c2800ee8397311c08f7c568
messier102 <messier102@fastmail.com>
2021-05-14 11:46:40 +0700

    Implement clearing out old posts on opengateaux

--- src/commands/commands/opengateaux.ts
+++ src/commands/commands/opengateaux.ts
@@ -25,17 +25,30 @@ export default <Command>{
         }
 
         try {
-            const post_id = await reddit.subreddits.postLink(
+            const bot_user = await reddit.users.fetchMe();
+            const old_posts = bot_user.getPosts();
+
+            for await (const old_post of old_posts) {
+                // A workaround for a bug in snoots which causes `getPosts()` to
+                // yield `undefined` if the user has no submissions.
+                if (!old_post) {
+                    continue;
+                }
+
+                await old_post.delete();
+            }
+
+            const new_post_id = await reddit.subreddits.postLink(
                 config.reddit.opengateaux_subreddit,
                 post_title,
                 invite.url
             );
 
-            const post = await reddit.posts.fetch(post_id);
-            await post.unmarkNsfw();
+            const new_post = await reddit.posts.fetch(new_post_id);
+            await new_post.unmarkNsfw();
 
             source.channel.send(
-                `Opened the gates: https://www.reddit.com${post.permalink}`
+                `Opened the gates: https://www.reddit.com${new_post.permalink}`
             );
         } catch (reason) {
             source.reply(`Reddit error: \`${reason.toString()}\``);
