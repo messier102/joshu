513b2cc44da4727e73e5009fbac4154235fe8fdd
messier102 <messier102@fastmail.com>
2021-05-19 14:20:33 +0700

    Fix typo

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,7 @@
  * Creates an iterable range of numeric values from `start` (inclusive)
  * to `end` (exclusive). Useful for defining for-loops.
  *
- * @param start The starting of the range (inclusive)
+ * @param start The starting value of the range (inclusive)
  * @param end The ending value of the range (exclusive)
  * @param step The distance between two consequtive values (default 1).
  *
