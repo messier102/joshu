560e269feb373517607add6a0214e7f4f8703109
messier102 <messier102@fastmail.com>
2021-05-28 11:19:11 +0700

    Document `zip`

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,15 @@
 /**
- * Creates an iterable range of numeric values from `start` (inclusive)
- * to `end` (exclusive). Useful for defining for-loops.
+ * Creates an iterable range of numeric values from `start` (inclusive) to `end`
+ * (exclusive). Useful for defining for-loops.
  *
- * @param start The starting value of the range (inclusive)
- * @param end The ending value of the range (exclusive)
- * @param step The distance between two consequtive values (default 1).
+ * @param start - Starting value of the range (inclusive).
+ * @param end - Ending value of the range (exclusive).
+ * @param step - Distance between two consequtive values (default 1).
  *
  * @example
+ * ```ts
  * for (const i of range(0, 5)) { ... }
+ * ```
  */
 export function* range(
     start: number,
@@ -19,6 +21,25 @@ export function* range(
     }
 }
 
+/**
+ * Creates a pairwise iterator over the elements of two arrays.
+ *
+ * Unlike the lodash `zip` which pads the shorter array of the two with
+ * `undefined`, causing the element type to become nullable, this version is
+ * modeled after Rust's `zip` and produces `min(left.length, right.length)`
+ * pairs, guaranteeing non-nullable types.
+ *
+ * @param left First array to iterate over.
+ * @param right Second array to iterate over.
+ *
+ * @example
+ * ```ts
+ * const left = [1, 2, 3]
+ * const right = ["a", "b", "c"]
+ *
+ * for (const [digit, char] of zip(left, right)) { ... }
+ * ```
+ */
 export function* zip<T, U>(
     left: readonly T[],
     right: readonly U[]
