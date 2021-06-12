5eeba97da9abee4eb2879c72ee775228c56e93eb
messier102 <messier102@fastmail.com>
2021-06-12 17:26:56 +0700

    Implement basic 8ball functionality

--- src/commands/commands/8ball.ts
+++ src/commands/commands/8ball.ts
@@ -3,6 +3,7 @@ import { Command, CommandParameter } from "../command";
 import { ValidatedCommandRequest } from "../request";
 import { CommandResponseOk } from "../response";
 import StringConverter from "../type_converters/StringConverter";
+import { sample } from "lodash";
 
 export default new Command(
     {
@@ -12,18 +13,49 @@ export default new Command(
         accept_remainder_arg: true,
     },
 
-    async (_: ValidatedCommandRequest, _question: string) => {
-        // we don't actually care about the question lol
-        return new EightBallResponse("Ask me later.");
+    async ({ source }: ValidatedCommandRequest, question: string) => {
+        const random_answer = sample(POSSIBLE_ANSWERS) as string;
+
+        return new EightBallResponse(
+            question,
+            source.author.username,
+            random_answer
+        );
     }
 );
 
 class EightBallResponse extends CommandResponseOk {
-    constructor(public readonly answer: string) {
+    constructor(
+        public readonly question: string,
+        public readonly asker: string,
+        public readonly answer: string
+    ) {
         super();
     }
 
     to_embed(): MessageEmbed {
-        return super.to_embed().setDescription(`🎱 ${this.answer}`);
+        return super
+            .to_embed()
+            .addField(`❓ ${this.asker} asks`, this.question)
+            .addField("🎱 8-ball says", this.answer);
     }
 }
+
+const POSSIBLE_ANSWERS = [
+    "Yes",
+    "Yes, definitely",
+    "As I see it, yes",
+    "Signs point to yes",
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "You may rely on it",
+    "Most likely",
+    "Outlook good",
+    "No",
+    "My reply is no",
+    "My sources say no",
+    "Don't count on it",
+    "Outlook not so good",
+    "Very doubtful",
+];
