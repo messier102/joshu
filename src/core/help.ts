b31cdc2c1fde1f00b183241ee441d6ed3069a6d4
messier102 <messier102@fastmail.com>
2021-06-26 20:26:06 +0700

    Automatically render permissions in command help

--- src/core/help.ts
+++ src/core/help.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed } from "discord.js";
+import { MessageEmbed, PermissionResolvable, Permissions } from "discord.js";
 import { sample } from "lodash";
 import config from "../../data/config";
 import { AnyCommand, Command, CommandMetadata } from "./command";
@@ -84,6 +84,13 @@ class CommandHelp extends ResponseHelp {
             embed.addField("Parameters", parameters);
         }
 
+        if (this.meta.permissions?.length > 0) {
+            embed.addField(
+                "Permissions required",
+                this.format_permissions(this.meta.permissions)
+            );
+        }
+
         if (this.meta.aliases) {
             const aliases = this.format_aliases(this.meta.aliases);
             embed.addField("Aliases", aliases);
@@ -130,6 +137,57 @@ class CommandHelp extends ResponseHelp {
         const name_normalized = name.split(" ").join("-");
 
         return `**${name_normalized}** \u2014 (${type}) ${description}`;
+    }
+
+    private format_permissions(permissions: PermissionResolvable[]): string {
+        const permission_names = new Map<PermissionResolvable, string>([
+            [Permissions.FLAGS.VIEW_CHANNEL, "View Channels"],
+            [Permissions.FLAGS.MANAGE_CHANNELS, "Manage Channels"],
+            [Permissions.FLAGS.MANAGE_ROLES, "Manage Roles"],
+            [Permissions.FLAGS.MANAGE_EMOJIS, "Manage Emojis"],
+            [Permissions.FLAGS.VIEW_AUDIT_LOG, "View Audit Log"],
+            [Permissions.FLAGS.MANAGE_WEBHOOKS, "Manage Webhooks"],
+            [Permissions.FLAGS.MANAGE_GUILD, "Manage Server"],
+            [Permissions.FLAGS.CREATE_INSTANT_INVITE, "Create Invite"],
+            [Permissions.FLAGS.CHANGE_NICKNAME, "Change Nickname"],
+            [Permissions.FLAGS.MANAGE_NICKNAMES, "Manage Nicknames"],
+            [Permissions.FLAGS.KICK_MEMBERS, "Kick Members"],
+            [Permissions.FLAGS.BAN_MEMBERS, "Ban Members"],
+            [Permissions.FLAGS.SEND_MESSAGES, "Send Messages"],
+            [Permissions.FLAGS.EMBED_LINKS, "Embed Links"],
+            [Permissions.FLAGS.ATTACH_FILES, "Attach Files"],
+            [Permissions.FLAGS.ADD_REACTIONS, "Add Reactions"],
+            [Permissions.FLAGS.USE_EXTERNAL_EMOJIS, "Use External Emoji"],
+            [
+                Permissions.FLAGS.MENTION_EVERYONE,
+                "Mention @everyone, @here and All Roles",
+            ],
+            [Permissions.FLAGS.MANAGE_MESSAGES, "Manage Messages"],
+            [Permissions.FLAGS.READ_MESSAGE_HISTORY, "Read Message History"],
+            [
+                Permissions.FLAGS.SEND_TTS_MESSAGES,
+                "Send Text-to-Speech Messages",
+            ],
+            // USE_SLASH_COMMANDS is not defined in the current version of discord.js
+            // [Permissions.FLAGS.USE_SLASH_COMMANDS , "Use Slash Commands"],
+            [Permissions.FLAGS.CONNECT, "Connect"],
+            [Permissions.FLAGS.SPEAK, "Speak"],
+            [Permissions.FLAGS.STREAM, "Video"],
+            [Permissions.FLAGS.USE_VAD, "Use Voice Activity"],
+            [Permissions.FLAGS.PRIORITY_SPEAKER, "Priority Speaker"],
+            [Permissions.FLAGS.MUTE_MEMBERS, "Mute Members"],
+            [Permissions.FLAGS.DEAFEN_MEMBERS, "Deafen Members"],
+            [Permissions.FLAGS.MOVE_MEMBERS, "Move Members"],
+            [Permissions.FLAGS.ADMINISTRATOR, "Administrator"],
+        ]);
+
+        return permissions
+            .map(
+                (p) =>
+                    permission_names.get(p) ??
+                    `<unrecognized permission id: ${p}>`
+            )
+            .join(", ");
     }
 
     private format_aliases(aliases: string[]): string {
