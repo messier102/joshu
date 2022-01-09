fe6d56961738cf621bf6b30f0bb7777dd0119696
messier102 <messier102@yandex.ru>
2022-01-09 17:03:46 +0700

    Deploy to correct user

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,13 +13,12 @@ module.exports = {
             key: "./production.key",
             ref: "origin/master",
             repo: "git@github.com:messier102/joshu.git",
-            path: "/home/ubuntu/joshu",
+            path: `/home/${process.env.PROD_USER}/joshu`,
             "pre-deploy": "node -v && npm -v",
-            "post-deploy":
-                "mkdir -p ./data && \
-                cp /home/ubuntu/joshu/shared/config.ts ./data/config.ts && \
+            "post-deploy": `mkdir -p ./data && \
+                cp /home/${process.env.PROD_USER}/joshu/shared/config.ts ./data/config.ts && \
                 npm run build-production && \
-                pm2 reload ecosystem.config.js --env production",
+                pm2 reload ecosystem.config.js --env production`,
         },
     },
 };
